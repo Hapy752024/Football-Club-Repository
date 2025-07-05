4db66beb7c05b2617f32f2507d078b09c13fae74
Hapy752024 <hapy752024@gmail.com>
2025-07-05 23:29:17 +0200

    Adjustments made  based on MS Tutorial

--- backend/crud/urls.py
+++ backend/crud/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
 
 urlpatterns = [
+    path("", lambda request: HttpResponse("Django backend is running!")),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
