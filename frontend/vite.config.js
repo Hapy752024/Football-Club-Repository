253a763747db331a2617a9c7d6f65de72e67630c
Hapy752024 <hapy752024@gmail.com>
2025-07-06 14:53:30 +0200

    Adjusted the vite build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensures consistency with GitHub workflow
+    assetsDir: 'assets', // Optional: organizes static assets
+    sourcemap: false, // Optional: disables source maps in prod
+  }
 })
